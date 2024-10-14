be65414bbb13b56818bb552c70281c0dd9e84499
unlie9 <unlie.devops9@gmail.com>
2024-10-14 17:20:57 +0300

    changed tag serializer and add tag info in productserializer

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
+from tag.serializers import TagSerializer
 from product.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
     category = serializers.CharField(source="category.name", read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
     class Meta:
         model = Product
         fields = ("id", "name", "price", "category", "tags")
